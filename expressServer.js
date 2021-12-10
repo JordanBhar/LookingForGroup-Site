2b78af67c1061f3b0cd65a92d9632e7d50152558
Jordan Bhar <bharjor@sheridancollege.ca>
2021-12-10 14:49:02 -0500

    Commit 9: Created post request so the posts.json will be updated whenever a post is created, going to work on the filter page now

--- expressServer.js
+++ expressServer.js
@@ -2,6 +2,8 @@ const path = require('path');
 const fs = require('fs')
 const express = require("express")
 const app = express()
+let postCreationAlert = require('alert');
+const { status } = require('express/lib/response');
 //https://stackoverflow.com/questions/48248832/stylesheet-not-loaded-because-of-mime-type used this to figure out how to get CSS and JavaScript validation back {learning}
 app.use(express.static(path.join(__dirname, 'CSS')));
 app.use(express.static(path.join(__dirname, 'JavaScript')));
@@ -13,5 +15,33 @@ app.get("/DragonrealmPostLFG", function (req, res) { res.sendFile(`${__dirname}/
 
 app.get("/InformationPage", function (req, res) { res.sendFile(`${__dirname}/views/InformationPage.html`) });
 
+
+app.post("/DragonrealmPostLFG", function(req , res){
+
+    let newPost = { 
+    "PostTitle":  req.body.title, 
+    "ActivityDescription": req.body.description, 
+    "AmountOfPlayers": req.body.playerNumber, 
+    "LevelReq": req.body.levelRequirement, 
+    "ActivityType":  req.body.activityType , 
+    "PlayerOne": req.body.playerType1, 
+    "PlayerTwo": req.body.playerType2,
+     "PlayerThree": req.body.playerType3,
+      "PlayerFour": req.body.playerType4};
+    //read json file
+    fs.readFile('data/posts.json', function(err, content){
+        let curObj = JSON.parse(content);
+        console.log(curObj)
+        curObj.createdPosts.push(newPost);
+        const finalString = JSON.stringify(curObj);
+        fs.writeFile('data/posts.json', finalString, function(err){
+            if(err) throw err;
+            //https://www.youtube.com/watch?v=p2JI2PrvleU
+            postCreationAlert("Created Post")
+            //res.send("post Created")
+            res.redirect('/DragonrealmPostLFG') 
+        });
+    });
+} );
 
 app.listen(5556);
