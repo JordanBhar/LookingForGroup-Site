0db27c27bac0769de072cc1a25f38f69320e86a6
Jordan Bhar <bharjor@sheridancollege.ca>
2021-12-10 13:41:24 -0500

    Commit 8: added the get request for express server so we can see the lfg page, also had to do some research to get the stycles and javascript for form validation working

--- expressServer.js
+++ expressServer.js
@@ -1,14 +1,17 @@
-const { response } = require('express');
+const path = require('path');
+const fs = require('fs')
 const express = require("express")
 const app = express()
-const postAddModule = require("./local_modules/postAddModule")
-app.use(express.urlencoded({ extended: true })) 
+//https://stackoverflow.com/questions/48248832/stylesheet-not-loaded-because-of-mime-type used this to figure out how to get CSS and JavaScript validation back {learning}
+app.use(express.static(path.join(__dirname, 'CSS')));
+app.use(express.static(path.join(__dirname, 'JavaScript')));
+app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.urlencoded({ extended: true }))
 
 
+app.get("/DragonrealmPostLFG", function (req, res) { res.sendFile(`${__dirname}/views/LookingForGroupForum.html`) });
 
-
-
-
+app.get("/InformationPage", function (req, res) { res.sendFile(`${__dirname}/views/InformationPage.html`) });
 
 
 app.listen(5556);
